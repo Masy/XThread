a0788382fda80ab5b6d07563f7c7e504456a6c61
Marcel Schauer <dev@masy.pw>
2019-03-09 14:22:17 +0100

    Added missing javadoc

--- src/main/java/pw/masy/xthread/crash/ICrashManager.java
+++ src/main/java/pw/masy/xthread/crash/ICrashManager.java
@@ -3,10 +3,19 @@ package pw.masy.xthread.crash;
 import java.text.SimpleDateFormat;
 import pw.masy.xthread.IXThread;
 
+/**
+ * Base interface for all crash managers.
+ */
 public interface ICrashManager {
 
 	SimpleDateFormat DATE = new SimpleDateFormat("YYYY-MM-dd-HH-mm-ss");
 
+	/**
+	 * Handles a crash report.
+	 *
+	 * @param thread    the {@link IXThread} the crash happened in
+	 * @param throwable the {@link Throwable} that caused the crash
+	 */
 	void handleCrash(IXThread thread, Throwable throwable);
 
 }
